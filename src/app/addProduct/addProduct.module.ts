a5cf67607c61176170185d274c4a784b2a5a320f
karthis668 <karthis668@gmail.com>
2021-06-17 09:54:32 +0530

    minor design changes

--- src/app/addProduct/addProduct.module.ts
+++ src/app/addProduct/addProduct.module.ts
@@ -2,7 +2,7 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab3Page } from './addProduct.page';
 
 import { Tab3PageRoutingModule } from './addProduct-routing.module';
@@ -14,6 +14,7 @@ import { Tab3PageRoutingModule } from './addProduct-routing.module';
     FormsModule,
     RouterModule.forChild([{ path: '', component: Tab3Page }]),
     Tab3PageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [Tab3Page]
 })
--- src/app/addProduct/addProduct.page.ts
+++ src/app/addProduct/addProduct.page.ts
@@ -1,4 +1,9 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import { NetworkService } from '../services/network.service';
+import { ToastService } from '../services/toast.service';
 
 @Component({
   selector: 'app-addproduct',
@@ -7,6 +12,61 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  addproductForm = new FormGroup({
+    name: new FormControl(),
+    shortDescription: new FormControl('11'),
+    reference: new FormControl(),
+    description: new FormControl(),
+    weight: new FormControl(),
+    price: new FormControl(),
+    categoryId: new FormControl(),
+    productType: new FormControl(),
+    qty: new FormControl(),
+    sku: new FormControl(),
+    // reference: new FormControl(),
+    // reference: new FormControl(),
+    // reference: new FormControl(),
+    // reference: new FormControl(),
+    // reference: new FormControl(),
+
+});
+
+  constructor(
+    private apiservice: ApiService,
+    private alertService: ToastService,
+    private internet: NetworkService,
+  ) {}
+
+  async addproduct() {
+    const formValue = this.addproductForm.value;
+    console.log(formValue);
+
+    this.internet.getStatus();
+    const addedProduct = {
+      data: {
+        sku: 'vfer34rde',
+        name: 'Sasdewmsung cv50a ',
+        description: 'check',
+        'short description': 'shorter',
+        weight: 1.1,
+        category_id: '6',
+        visibility: 1,
+        price: 150000,
+        url_key: 'samsunglite',
+        qty: 10,
+        product_type: 'simple'
+      }
+    };
+    // this.apiservice.addProduct(addedProduct).subscribe(
+    //   res => {
+    //   },
+    //     error => {
+    //       this.internet.getStatus();
+    //       // this.loadingService.dismiss();
+    //       localStorage.isprofileUpdated = 'no';
+    //       console.log(error);
+    //       this.alertService.presentToast(error.error.messsage);
+    //     });
+  }
 
 }
